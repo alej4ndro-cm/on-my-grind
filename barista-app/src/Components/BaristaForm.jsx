e58c2899d3d8c9aa5068f1222480445ace10ce0e
C0cuy0 <am.private.g@gmail.com>
2025-02-05 22:15:17 -0700

    Fixed missing buttons in BaristaForm

--- barista-app/src/Components/BaristaForm.jsx
+++ barista-app/src/Components/BaristaForm.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const BaristaForm = () => {
-  return <div></div>;
+  const onNewDrink = () => {};
+  const onCheckAnswer = () => {};
+
+  return (
+    <div>
+      <h2>Hi, I'd like to order a:</h2>
+      <form>
+        {/* Inputs will be added in the next step */}
+      </form>
+
+      <button type="submit" className="button submit" onClick={onCheckAnswer}>
+        Check Answer
+      </button>
+
+      <button
+        type="button"
+        className="button newdrink"
+        onClick={onNewDrink}
+      >
+        New Drink
+      </button>
+    </div>
+  );
 };
 
 export default BaristaForm;
