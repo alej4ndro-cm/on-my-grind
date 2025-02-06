03c3ba5529b0146dc369058a2e2409b558c79812
C0cuy0 <am.private.g@gmail.com>
2025-02-06 00:31:41 -0700

    Adding stretch features

--- barista-app/src/Components/RecipeChoices.jsx
+++ barista-app/src/Components/RecipeChoices.jsx
@@ -1,22 +1,25 @@
-import React, { Component, useEffect, useState } from "react";
+import React from "react";
 
 const RecipeChoices = ({ handleChange, label, choices, checked }) => {
     return (
       <div className="radio-buttons">
+        {/* Add text input at the top */}
+        <input
+          type="text"
+          name={label}
+          value={checked}
+          placeholder="Guess the ingredient..."
+          onChange={handleChange}
+          className="textbox"
+        />
+        
+        {/* List choices below */}
         {choices &&
           choices.map((choice) => (
             <li key={choice}>
-              <input
-                id={choice}
-                value={choice}
-                name={label}
-                type="radio"
-                onChange={handleChange}
-                checked={checked == choice}
-              />
-                {choice}
+              {choice}
             </li>
-          ))}
+        ))}
       </div>
     );
 };
